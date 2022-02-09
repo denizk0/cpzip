7095776a398e094ea6c8b54da0787dadd2b8c517
denizk0 <razzorro@gmail.com>
2022-02-09 16:47:12 +0300

    First version

---
+++ Options.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.CodeAnalysis;
+using CommandLine;
+using CommandLine.Text;
+
+namespace cpzip;
+
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+internal class Options
+{
+    [Value(0, Required = true, HelpText = "Source file to copy.", MetaName = "source_file")]
+    public string SourceFile { get; set; }
+
+    [Value(1, Required = true, HelpText = "Target file to copy to.", MetaName = "target_file")]
+    public string TargetFile { get; set; }
+
+    [Value(2, Required = true, HelpText = "Path within the target file. Use '/' as a separator.", MetaName = "target_path")]
+    public string TargetFilePath { get; set; }
+
+    [Option('n', "no-overwrite", Required = false, HelpText = "Do not overwrite existing files.")]
+    public bool NoOverwrite { get; set; }
+
+    [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+    public bool Verbose { get; set; }
+
+    
+    internal static void DisplayHelp<T>(ParserResult<T> result)
+    {  
+        var helpText = HelpText.AutoBuild(result, h =>
+        {
+            h.AdditionalNewLineAfterOption = false;
+            h.Copyright = string.Empty;
+            h.Heading =
+                "Copies file to the target zip archive, with nested archives support.";
+            h.AddPreOptionsLine("Usage: cpzip source_file target_file target_path [options]");
+            h.AddPostOptionsLine("Example:");
+            h.AddPostOptionsLine("cpzip my_photo.png my_photos.zip christmas/this_year.zip/new");
+            h.AddPostOptionsLine("will copy 'my_photo.png' to the folder 'new' of the nested file 'this_year.zip' updating 'my_photos.zip' accordingly");
+            
+            return HelpText.DefaultParsingErrorsHandler(result, h);
+        }, e => e);
+        Console.WriteLine(helpText);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System.IO.Compression;
+using System.Text.RegularExpressions;
+using CommandLine;
+using cpzip;
+
+const string zipSeparator = "/";
+
+#region Logging
+
+bool verbose;
+
+void Log(object message)
+{
+    if (verbose)
+    {
+        Console.WriteLine(message?.ToString());
+    }
+}
+
+void LogError(object message) => Console.Error.WriteLine($"ERROR: {message}");
+
+#endregion
+
+#region Command line processing
+
+FileInfo sourceFile, targetFile;
+bool noOverwrite;
+
+var parser = new Parser(with => with.HelpWriter = null);
+var parserResult = parser.ParseArguments<Options>(args);
+
+return parserResult.MapResult(o =>
+{
+    try
+    {
+        sourceFile = new FileInfo(o.SourceFile);
+        targetFile = new FileInfo(o.TargetFile);
+
+        if (!sourceFile.Exists)
+        {
+            throw new FileNotFoundException($"Source file not found: {sourceFile.FullName}.", o.SourceFile);
+        }
+
+        if (!targetFile.Exists)
+        {
+            throw new FileNotFoundException($"Target file not found: {targetFile.FullName}.", o.TargetFile);
+        }
+
+        noOverwrite = o.NoOverwrite;
+        verbose = o.Verbose;
+
+        Process(targetFile.FullName, o.TargetFilePath);
+        return 0;
+    }
+    catch (Exception e)
+    {
+        LogError(e.Message);
+        return e.HResult != 0 ? 1 : e.HResult;
+    }
+}, _ =>
+{
+    Options.DisplayHelp(parserResult);
+    return -1;
+});
+
+#endregion
+
+void Process(string zipFilePath, string targetPath)
+{
+    using var zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Update);
+
+    var zipPath = string.Empty;
+    ZipArchiveEntry zipEntry = null;
+
+    foreach (var path in targetPath.Split(zipSeparator, StringSplitOptions.RemoveEmptyEntries))
+    {
+        zipPath += path;
+        zipEntry = zipArchive.GetEntry(zipPath);
+
+        // not a file
+        if (zipEntry == null)
+        {
+            zipPath += zipSeparator;
+            zipEntry = zipArchive.GetEntry(zipPath);
+
+            // not a directory
+            if (zipEntry == null)
+            {
+                throw new FileNotFoundException($"Entry {zipFilePath}{zipSeparator}{zipPath} not found. Check target path.");
+            }
+        }
+        // it's a file
+        else
+        {
+            var tempFileName = Path.GetTempFileName();
+            try
+            {
+                Log($"Processing nested zip {zipPath}...");
+                Log($"Extracting nested zip {zipFilePath}{zipSeparator}{zipPath} to {tempFileName}...");
+                zipEntry.ExtractToFile(tempFileName, true);
+
+                // trim processed path
+                var remainingPath = new Regex(Regex.Escape(zipPath)).Replace(targetPath, string.Empty, 1);
+
+                Process(tempFileName, remainingPath);
+
+                Log($"Deleting existing nested entry {zipFilePath}{zipSeparator}{zipEntry}...");
+                zipEntry.Delete();
+
+                Log($"Creating nested entry {zipFilePath}{zipSeparator}{zipPath} from {tempFileName}...");
+                zipArchive.CreateEntryFromFile(tempFileName, zipPath);
+
+                return;
+            }
+            finally
+            {
+                Log($"Deleting {tempFileName}...");
+                File.Delete(tempFileName);
+            }
+        }
+    }
+
+    var entryName = (zipEntry?.FullName ?? string.Empty) + sourceFile.Name;
+    var entry = zipArchive.GetEntry(entryName);
+    if (entry != null)
+    {
+        if (noOverwrite)
+        {
+            throw new ApplicationException($"Entry {entryName} already exists.");
+        }
+
+        entry.Delete();
+        Log($"Deleting existing entry {zipFilePath}{zipSeparator}{entry}...");
+    }
+
+    Log($"Creating entry {zipFilePath}{zipSeparator}{entryName}...");
+    zipArchive.CreateEntryFromFile(sourceFile.FullName, entryName);
+}
